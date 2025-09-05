22bc3a4b5cd640e79a7fbce0df947b6622a0b7c7
hehiboy <viquocanhh@gmail.com>
2025-09-05 16:11:57 +0700

    FFnhucut.lua

---
+++ FFnhucut.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ninja10908/S4/refs/heads/main/Kurdhub"))()
+wait(2)
+loadstring(game:HttpGet("https://rawscripts.net/raw/Steal-a-Brainrot-Lennon-hub-made-by-kaioewqasn-51389"))()
