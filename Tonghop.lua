6938693641f09f531f84375df8409369cfe8a8c8
hehiboy <viquocanhh@gmail.com>
2025-09-14 14:52:33 +0700

    Create Tonghop.lua

---
+++ Tonghop.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hehiboy/H/main/fps.lua"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hehiboy/H/main/Ngumoidung.lua"))()
